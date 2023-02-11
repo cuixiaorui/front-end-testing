a5ae073335e56a758762119a27d848db5d5cb479
cuixiaorui <cui_xiaorui@126.com>
2023-02-11 22:19:50 +0800

    feat: add advertising view

---
+++ docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,33 @@
+<script setup>
+import DefaultTheme from "vitepress/theme";
+
+const { Layout } = DefaultTheme;
+</script>
+
+<template>
+  <Layout>
+    <template #aside-ads-before>
+      <div class="advertising">
+        <div>
+          <p>课程即将开启预售</p>
+        </div>
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<style>
+.advertising {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+  border-radius: 12px;
+  min-height: 256px;
+  text-align: center;
+  line-height: 18px;
+  font-size: 12px;
+  font-weight: 500;
+  background-color: rgb(25, 25, 29);
+}
+</style>
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,7 @@
+import DefaultTheme from 'vitepress/theme'
+import MyLayout from './MyLayout.vue'
+
+export default {
+  ...DefaultTheme,
+  Layout: MyLayout
+}
